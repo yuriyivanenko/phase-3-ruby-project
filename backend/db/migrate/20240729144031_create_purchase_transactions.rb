7ce9af190d70a9b042b74e46afbdc189df8d7a8f
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-07-29 11:28:25 -0400

    Rename purchase_transactions table columns. Rollback and rerun migrations

--- backend/db/migrate/20240729144031_create_purchase_transactions.rb
+++ backend/db/migrate/20240729144031_create_purchase_transactions.rb
@@ -3,7 +3,7 @@ class CreatePurchaseTransactions < ActiveRecord::Migration[6.1]
     create_table :purchase_transactions do |t|
       t.decimal :amount, precision: 10, scale: 2
       t.string :description
-      t.integer :category_id
+      t.integer :purchase_category_id
       t.integer :vendor_id
       t.integer :user_id
       t.timestamps
