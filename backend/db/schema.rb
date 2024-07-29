fcc861c282c581f3b2b8cd361df2d5ddd530a28f
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-07-29 11:31:45 -0400

    Change misc items

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,12 +10,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_07_29_140832) do
+ActiveRecord::Schema.define(version: 2024_07_29_151531) do
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "purchase_categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+  end
+
+  create_table "purchase_transactions", force: :cascade do |t|
+    t.decimal "amount", precision: 10, scale: 2
+    t.string "description"
+    t.integer "purchase_category_id"
+    t.integer "vendor_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "sales_categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+  end
+
+  create_table "sales_transactions", force: :cascade do |t|
+    t.decimal "amount", precision: 10, scale: 2
+    t.string "description"
+    t.integer "sales_category_id"
+    t.integer "customer_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "business_name"
     t.string "username"
     t.string "password"
+  end
+
+  create_table "vendors", force: :cascade do |t|
+    t.string "vendor_name"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
