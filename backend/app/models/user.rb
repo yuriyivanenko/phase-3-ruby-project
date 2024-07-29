2ab350d633ea34dc5394b00120771fe32627eda7
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-07-29 11:29:13 -0400

    Add all data associations

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_many :vendors
   has_many :purchase_categories
   has_many :purchase_transactions
+  has_many :customers
+  has_many :sales_categories
+  has_many :sales_transactions
 end
